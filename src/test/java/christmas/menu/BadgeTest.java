5a81295e5eee5deba2037f1b3f508db497a8b001
Soonrang <135775059+Soonrang@users.noreply.github.com>
2023-11-13 03:20:52 +0900

    Update BadgeTest.java

--- src/test/java/christmas/menu/BadgeTest.java
+++ src/test/java/christmas/menu/BadgeTest.java
@@ -12,13 +12,10 @@ public class BadgeTest {
     public void testPresentBadge() {
         assertEquals("트리", Badge.presentBadge(15000));
 
-        // 테스트 케이스 2: 1000원인 경우 (조건을 만족하지 않아서 null이 반환되어야 함)
         assertNull(Badge.presentBadge(1000));
 
-        // 테스트 케이스 3: 10000원인 경우
         assertEquals("트리", Badge.presentBadge(10000));
 
-        // 테스트 케이스 4: 25000원인 경우
         assertEquals("산타", Badge.presentBadge(20000));
 
     }
