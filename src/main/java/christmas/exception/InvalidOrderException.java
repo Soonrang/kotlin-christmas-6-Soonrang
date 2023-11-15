f652c72cd422b1ccd0f21dac3a050063faa357b1
Soonrang <clearsbyul30@naver.com>
2023-11-15 23:39:39 +0900

    feat: 불가능한 주문 예외처리

---
+++ src/main/java/christmas/exception/InvalidOrderException.java
@@ -0,0 +1,11 @@
+package christmas.exception;
+
+public class InvalidOrderException extends IllegalArgumentException{
+
+    private static final String ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+    public InvalidOrderException(){
+        super(ERROR_MESSAGE);
+    }
+
+}
