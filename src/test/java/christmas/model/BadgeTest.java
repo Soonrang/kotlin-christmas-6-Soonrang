500abadb9a06da95f2c28fda4a305750210698a8
Soonrang <clearsbyul30@naver.com>
2023-11-15 23:38:21 +0900

    feat: 뱃지 부여 테스트

--- src/test/java/christmas/model/BadgeTest.java
+++ src/test/java/christmas/model/BadgeTest.java
@@ -1,9 +1,7 @@
-package christmas.menu;
+package christmas.model;
 
-import christmas.model.Badge;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
